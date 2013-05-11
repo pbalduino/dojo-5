3df46d65eddca21f6be4c6e8697c0ac921f119cc
Plinio Balduino <pbalduino+github@gmail.com>
2013-05-11 11:58:48 -0300

    deixando o código mais legível

--- src/spock/core.clj
+++ src/spock/core.clj
@@ -9,10 +9,10 @@
 
 (defn jokenpo [jogador1 jogador2]
   (let [jogadas-validas (jogadas jogador1)
-  	    jogador1-vence  (some #(= jogador2 %) 
-  	    	                    jogadas-validas)]
-    (if (= jogador2 jogador1) 
-        :empate
-        (if jogador1-vence
-            :jogador1
-            :jogador2))))
+        jogador1-vence  (some #(= jogador2 %) 
+                              jogadas-validas)
+        jogadas-iguais  (= jogador2 jogador1)]
+
+    (cond jogadas-iguais :empate
+          jogador1-vence :jogador1
+          :else          :jogador2)))
