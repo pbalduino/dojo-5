2b5a9a3287d0c7dccd8503a5bc1b7be01aefd67e
Plinio Balduino <pbalduino+github@gmail.com>
2013-05-11 12:05:19 -0300

    Adicionados comentários inline para deixar o código mais claro ao iniciante

--- src/spock/core.clj
+++ src/spock/core.clj
@@ -1,18 +1,18 @@
 (ns spock.core)
 
-(def jogadas 
-  {:tesoura [:lagarto :papel]
-   :papel   [:spock   :pedra]
-   :pedra   [:tesoura :lagarto]
+(def jogadas                                   ; jogadas é um hash/dicionário
+  {:tesoura [:lagarto :papel]                  ; que contém as jogadas válidas 
+   :papel   [:spock   :pedra]                  ; e as possibilidades que levam
+   :pedra   [:tesoura :lagarto]                ; à vitória
    :spock   [:pedra   :tesoura]
    :lagarto [:spock   :papel]})
 
 (defn jokenpo [jogador1 jogador2]
-  (let [jogadas-validas (jogadas jogador1)
-        jogador1-vence  (some #(= jogador2 %) 
-                              jogadas-validas)
-        jogadas-iguais  (= jogador1 jogador2)]
+  (let [jogadas-validas (jogadas jogador1)     ; let cria um escopo local onde 
+        jogador1-vence  (some #(= jogador2 %)  ; eu crio os valores (bindings)
+                              jogadas-validas) ; que podem ser usados como
+        jogadas-iguais  (= jogador1 jogador2)] ; constantes no código
 
-    (cond jogadas-iguais :empate
-          jogador1-vence :jogador1
+    (cond jogadas-iguais :empate               ; assim o cond fica fácil de ler
+          jogador1-vence :jogador1             ; =)
           :else          :jogador2)))
